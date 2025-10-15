11349d4f8e1be3e58bc577f4f3f71ca2ae374319
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2025-10-15 13:06:26 +0100

    Passenger controller is completed, errors fixed.

--- src/main/java/ie/atu/week5lab3/controller/PassengerController.java
+++ src/main/java/ie/atu/week5lab3/controller/PassengerController.java
@@ -1,4 +1,48 @@
 package ie.atu.week5lab3.controller;
 
+
+import ie.atu.week5lab3.model.Passenger;
+import ie.atu.week5lab3.service.PassengerService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/passenger")
 public class PassengerController {
+
+    private final PassengerService passengerService;
+
+    public PassengerController(PassengerService passengerService) {
+        this.passengerService = passengerService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Passenger> getAll(@PathVariable String id) {
+
+        Optional<Passenger> maybe = passengerService.findById(id);
+
+        if (maybe.isPresent()) {
+            return ResponseEntity.ok(maybe.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Passenger> create(@Valid @RequestBody Passenger passenger) {
+
+        Passenger created = PassengerService.create(passenger);
+
+        return ResponseEntity
+                .created(URI.create(URI.create("/api/passenger/") + created.getPassengerID()))
+                .body(created);
+
+    }
 }
