1925282d0d7ad5e58aebaf12e5759ad5f66eee60
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2025-10-22 15:31:50 +0100

    Exception details class is created.

---
+++ src/main/java/errorHandling/ExceptionDetails.java
@@ -0,0 +1,15 @@
+package errorHandling;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionDetails {
+
+    private String fieldName;
+    private String fieldValue;
+}
